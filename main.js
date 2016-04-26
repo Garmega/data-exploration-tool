900d1c5991fc7516eedccf63ff1872b5d1f1506d
Garmega <garudaomega@gmail.com>
2016-04-26 06:18:15 -0700

    syntax fix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function main() {
     console.log("We are ready!");
     $.ajax({
         type: 'GET',
-        url: "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=C97C4727D2637174264FA388C7AF9C45&account_id=65406320"
+        url: "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=C97C4727D2637174264FA388C7AF9C45&account_id=65406320",
         data: { get_param: 'value'},
         dataType: 'json',
         success: function(data) {
