181647bf8906aa30e5d1f25f4a09a4c173d56ae6
Garmega <garudaomega@gmail.com>
2016-04-26 06:22:02 -0700

    grabbing local data

--- main.js
+++ main.js
@@ -5,17 +5,7 @@ $ ( document ).ready(function() {
 
 function main() {
     console.log("We are ready!");
-    $.ajax({
-        type: 'GET',
-        url: "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=C97C4727D2637174264FA388C7AF9C45&account_id=65406320",
-        data: { get_param: 'value'},
-        dataType: 'json',
-        success: function(data) {
-            $.each(data, function(index, element) {
-                $('body').append($('<div>', {
-                    text: element.name
-                }));
-            });
-        }
-    });
+    $.getJSON('playerdata.json', function(json) {
+        console.log(json);
+    })
 }
