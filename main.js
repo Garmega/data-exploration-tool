a8e29e8c5cfa404ec19c196b48a772498a6965be
Garmega <garudaomega@gmail.com>
2016-04-28 05:06:38 -0700

    added comments and reduced some redundancy

--- main.js
+++ main.js
@@ -27,11 +27,16 @@ function main() {
     }
 }
 
+/*
+Gets the hero for the match for this player.
+@parms match: The match for which the hero shall be extracted from
+@returns A hero object that the player played during this match
+*/
 function getHeroForMatch(match) {
     if (match.match_id != null) {
         var player = getPlayerInMatch(match);
-        if (player != null && player.length == 1) {
-            return getHeroById(player[0].hero_id)[0];
+        if (player != null) {
+            return getHeroById(player.hero_id);
         } else {
             //no player exists in that match OR
             //data is somehow corrupt and there are multiples
@@ -41,14 +46,30 @@ function getHeroForMatch(match) {
     return null;
 }
 
+/*
+Grabs the hero by id from the hero list.
+@params id: Id of the hero you wish to look for
+@returns The hero object if found. If multiples are found,only
+the first one is return. If none is found than null.
+*/
 function getHeroById(id) {
-    return $.grep(heroes, function(n,i) {
+    var results = $.grep(heroes, function(n,i) {
         return n.id == id
     });
+
+    return results[0];
 }
 
+/*
+Grabs the player from the match player list.
+@params match: The match that is going to be searched
+@returns The player object if found. If multiples are found,only
+the first one is return. If none is found than null.
+*/
 function getPlayerInMatch(match) {
-    return $.grep(match.players, function(n,i) {
+    var results = $.grep(match.players, function(n,i) {
         return n.account_id == accountId;
     });
+
+    return results[0];
 }
