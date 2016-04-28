2b9ef78b221f605588ac7c57fb27688765a0c0ef
Garmega <garudaomega@gmail.com>
2016-04-28 04:03:54 -0700

    added utility functions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var matchInfo;
-var heroInfo;
+var matches;
+var heroes;
+var accountId = "65406320";
 
 $ ( document ).ready(function() {
     main();
@@ -8,18 +9,38 @@ $ ( document ).ready(function() {
 
 function main() {
     console.log("We are ready!");
+    loadData();
+
+    for (var i = 0; i < 10; i++) {
+        getHeroForMatch(matches[i]);
+    }
+}
+
+function loadData() {
     $.getJSON('playerdata.json', function(json) {
-        matchInfo = json;
-        console.log(matchInfo);
+        matches = json.matches;
+        console.log(matches);
     })
 
     $.getJSON('herodata.json', function(json) {
-        heroInfo = json;
-        console.log(heroInfo);
+        heroes = json.heroes;
+        console.log(heroes);
     })
-
 }
 
-function getHeroForMatch() {
+function getHeroForMatch(match) {
+    if (match_id != null) {
+        var players = match.players;
+        for (var i = 0; i < players.length; i++) {
+            if (players.account_id == accountId) {
+                return getHeroById(players.hero_id);
+            }
+        }
+    }
 
+    return null;
+}
+
+function getHeroById(id) {
+    return heroes[id - 1];
 }
